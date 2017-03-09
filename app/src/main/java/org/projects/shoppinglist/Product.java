a74fc5e6bab57c7565d1e56aeb7531ef4a56cadb
terpster <ktn1710@gmail.com>
2017-03-09 16:39:28 +0100

    fragment added, not working yet

---
+++ app/src/main/java/org/projects/shoppinglist/Product.java
@@ -0,0 +1,52 @@
+package org.projects.shoppinglist;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Kasper on 09-03-2017.
+ */
+
+public class Product implements Parcelable{
+    String name;
+    int quantity;
+    public static final Parcelable.Creator<Product> CREATOR
+            = new Parcelable.Creator<Product>() {
+
+        // This simply calls our new constructor (typically private) and
+        // passes along the unmarshalled `Parcel`, and then returns the new object!
+        @Override
+        public Product createFromParcel(Parcel in) {
+            return new Product( );
+        }
+
+        // We just need to copy this and change the type to match our class.
+        @Override
+        public Product[] newArray(int size) {
+            return new Product[size];
+        }
+    };
+
+    public Product() {
+    }
+
+    public Product(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + quantity;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
